7799d787aed5b53e8003a32ecbdd4de92da37bd3
Arnold <204812868@qq.com>
2019-10-21 15:29:30 +0800

    修改

--- dsa5200web/module/fire/assets/libs/gismap/config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-const mapConfig = {
-	img_format: '.jpg',
-	tiles_dir: 'http://172.26.1.233:8085/webshare/MAPS/TILES', //瓦片路径（必须要有）
-	home: 'assets/libs/gismap/'
-}
